fbc3d2e4f84ff314c0816ed5cb9c913c42a4ddb3
Nerd <studytime023@gmail.com>
2025-02-19 02:13:03 +0300

    fix (URLS) : add pk for return book endpoint

--- borrowing_records/urls.py
+++ borrowing_records/urls.py
@@ -4,5 +4,5 @@ from .views import BorrowBook,ReturnBook,BorrowedBooks
 urlpatterns=[
     path("",BorrowedBooks.as_view(),name="borrowed-books"),
     path("borrow-book/",BorrowBook.as_view(),name="borrow-book"),
-    path("return-book/",ReturnBook.as_view(),name="return-book"),
+    path("<str:pk>/return-book/",ReturnBook.as_view(),name="return-book"),
 ]
